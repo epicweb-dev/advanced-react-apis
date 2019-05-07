c82a8e57ab15797cd9b628065a418dface99d44e
Kent C. Dodds <kent@doddsfamily.us>
2019-05-07 17:12:47 -0600

    ensure we only warn once

--- src/hacks.js
+++ src/hacks.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-// if there have been 20 calls of a given effect in 3 seconds then that's probably a runaway
+// if there have been 20 calls of a given effect in 2 seconds then that's probably a runaway
 const RECORDS = 20
-const TIME_LIMIT = 3000
+const TIME_LIMIT = 2000
 
 if (process.env.NODE_ENV !== 'production') {
   throttleEffectHook('useEffect')
@@ -13,11 +13,16 @@ function throttleEffectHook(hookName) {
   const originalHook = React[hookName]
   React[hookName] = function useThrottledHook(...args) {
     const ref = React.useRef([])
+    const warnedRef = React.useRef(false)
     const calls = ref.current
     const oldestCall = calls.slice(-1)[0]
     const now = Date.now()
     calls.push({time: now, args})
-    if (calls.length >= RECORDS && oldestCall.time > now - TIME_LIMIT) {
+    if (
+      !warnedRef.current &&
+      calls.length >= RECORDS &&
+      oldestCall.time > now - TIME_LIMIT
+    ) {
       const allRecentCallDependencies = calls.map(c => c.args[1])
       const messages = [
         `The following effect callback was invoked ${RECORDS} times in ${TIME_LIMIT}ms`,
@@ -43,6 +48,7 @@ function throttleEffectHook(hookName) {
         )
       }
       console.warn(...messages)
+      warnedRef.current = true
       throw new Error(
         `Uh oh... Looks like we've got a runaway ${hookName}. Check the console for more info. Make sure the ${hookName} is being passed the right dependencies. (Note, this error message is from Kent, not React 👋)`,
       )
