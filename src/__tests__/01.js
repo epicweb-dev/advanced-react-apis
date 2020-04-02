030291f599af2edffff0428479acdd61ad02ec1b
Mike Schutte <tmikeschutte@gmail.com>
2020-04-02 12:04:46 -0400

    Update 01.js

--- src/__tests__/01.js
+++ src/__tests__/01.js
@@ -2,7 +2,7 @@ import React from 'react'
 import chalk from 'chalk'
 import {render, fireEvent} from '@testing-library/react'
 import App from '../final/01'
-// import App from '../exercises/01'
+// import App from '../exercise/01'
 
 test('clicking the button increments the count', () => {
   const {container} = render(<App />)
