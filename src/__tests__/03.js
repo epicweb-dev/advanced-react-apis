83e0e8f6b49e6582062c0d7d93807b1bd4f1a221
Markus Lasermann <github@ml74.de>
2020-07-25 16:47:58 +0200

    Fix typo to be able to run the tests on files in src/exercise (#35)

--- src/__tests__/03.js
+++ src/__tests__/03.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {render, screen, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from '../final/03'
-// import App from '../exercises/03'
+// import App from '../exercise/03'
 
 test('clicking the button increments the count', () => {
   render(<App />)
