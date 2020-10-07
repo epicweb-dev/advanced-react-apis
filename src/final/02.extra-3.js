ed9b2826ed7461175285c003233ac6dbed4374fc
Kent C. Dodds <me+github@kentcdodds.com>
2020-10-07 12:01:09 -0600

    Update src/final/02.extra-3.js

--- src/final/02.extra-3.js
+++ src/final/02.extra-3.js
@@ -16,7 +16,7 @@ function useSafeDispatch(dispatch) {
 
   // to make this even more generic you should use the useLayoutEffect hook to
   // make sure that you are correctly setting the mountedRef.current immediately
-  // after React updates the Dom.  Even though this effect does not interact
+  // after React updates the DOM. Even though this effect does not interact
   // with the dom another side effect inside a useLayoutEffect which does
   // interact with the dom may depend on the value being set
   React.useEffect(() => {
