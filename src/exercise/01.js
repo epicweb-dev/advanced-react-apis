fd6afee9e12056ecb01028e4592a22d0790c33c5
Kent C. Dodds <me+github@kentcdodds.com>
2020-10-10 20:39:21 -0600

    improve the name action

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -11,7 +11,7 @@ function Counter({initialCount = 0, step = 1}) {
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "action" - the value passed to setCount
+  // The 2nd argument is called "newState" - the value passed to setCount
   const increment = () => setCount(count + step)
   return <button onClick={increment}>{count}</button>
 }
