9c31cacc13a067f237173d02944b0385de09b09c
Kent C. Dodds <me+github@kentcdodds.com>
2020-10-26 09:17:41 -0600

    fix tests for react 17
    Closes #88
    Closes #90

--- src/__tests__/01.js
+++ src/__tests__/01.js
@@ -1,46 +1,19 @@
 import React from 'react'
-import chalk from 'chalk'
+import {alfredTip} from '@kentcdodds/react-workshop-app/test-utils'
 import {render} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from '../final/01'
 // import App from '../exercise/01'
 
-test('clicking the button increments the count', () => {
+test('clicking the button increments the count with useReducer', () => {
+  jest.spyOn(React, 'useReducer')
+
   const {container} = render(<App />)
   const button = container.querySelector('button')
   userEvent.click(button)
   expect(button).toHaveTextContent('1')
-})
 
-// this test is using some serious witchcraft 🧙‍♀️
-// don't write tests like this please.
-// I'm just making sure that you're using useReducer
-// but your apps should not have tests like this.
-// That's an implementation detail... Read more: https://kcd.im/imp-deets
-test('using useReducer', () => {
-  const createElement = React.createElement
-  let counterFn
-  React.createElement = (...args) => {
-    if (args[0] && args[0].name === 'Counter') {
-      counterFn = args[0]
-    }
-    return createElement(...args)
-  }
-  render(<App />)
-  React.createElement = createElement
-
-  const lines = counterFn.toString().split('\n')
-  const useReducerLine = lines.find(line => {
-    return !line.trim().startsWith('//') && line.includes('useReducer(')
-  })
-  const useStateLine = lines.find(line => {
-    return !line.trim().startsWith('//') && line.includes('useState(')
-  })
-  if (!useReducerLine || useStateLine) {
-    throw new Error(
-      `🚨  ${chalk.red(
-        'The Counter component that is rendered must call "useReducer" to get the "state" and "dispatch" function and you should get rid of that useState call.',
-      )}`,
-    )
-  }
+  alfredTip(() => {
+    expect(React.useReducer).toHaveBeenCalled()
+  }, 'The Counter component that is rendered must call "useReducer" to get the "state" and "dispatch" function and you should get rid of that useState call.')
 })
--- src/__tests__/03.js
+++ src/__tests__/03.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render, screen, act} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from '../final/03'
 // import App from '../exercise/03'
@@ -13,37 +13,4 @@ test('clicking the button increments the count', () => {
   expect(display).toHaveTextContent(/1/)
   userEvent.click(button)
   expect(display).toHaveTextContent(/2/)
-})
-
-// this test is using some serious witchcraft 🧙‍♀️
-// don't write tests like this please.
-// I'm just making sure that you're using useReducer
-// but your apps should not have tests like this.
-// That's an implementation detail... Read more: https://kcd.im/imp-deets
-test('CountProvider is rendering a context provider with the right value', () => {
-  const createElement = React.createElement
-
-  const providerProps = {}
-  React.createElement = (...args) => {
-    if (args[0].$$typeof === Symbol.for('react.provider')) {
-      Object.assign(providerProps, args[1])
-      if (args.length > 2) {
-        providerProps.children = args.slice(2)
-      }
-    }
-    return createElement(...args)
-  }
-
-  render(<App />)
-
-  expect(providerProps.value).toEqual([0, expect.any(Function)])
-
-  act(() => {
-    providerProps.value[1](1) // lol
-  })
-
-  // assert that calling setCount directly updates the count state
-  expect(providerProps.value).toEqual([1, expect.any(Function)])
-
-  React.createElement = createElement
 })
