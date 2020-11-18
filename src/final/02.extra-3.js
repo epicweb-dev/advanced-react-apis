5557abab341db60835d16df9df75e8621b6797fb
Kent C. Dodds <me+github@kentcdodds.com>
2020-11-18 16:38:46 -0700

    improve 02 a bit

--- src/final/02.extra-3.js
+++ src/final/02.extra-3.js
@@ -131,4 +131,22 @@ function App() {
   )
 }
 
-export default App
+function AppWithUnmountCheckbox() {
+  const [mountApp, setMountApp] = React.useState(true)
+  return (
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          checked={mountApp}
+          onChange={e => setMountApp(e.target.checked)}
+        />{' '}
+        Mount Component
+      </label>
+      <hr />
+      {mountApp ? <App /> : null}
+    </div>
+  )
+}
+
+export default AppWithUnmountCheckbox
