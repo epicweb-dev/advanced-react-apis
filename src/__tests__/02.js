1bfe27742e068bbc8ed36f82b88708c7d946c4e6
Kate <test@gmail.com>
2021-01-15 16:16:30 -0500

    extractn pokenmanReducer as generic function

--- src/__tests__/02.js
+++ src/__tests__/02.js
@@ -2,8 +2,7 @@ import * as React from 'react'
 import {alfredTip} from '@kentcdodds/react-workshop-app/test-utils'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import App from '../final/02'
-// import App from '../exercise/02'
+import App from '../exercise/02'
 
 beforeEach(() => {
   jest.spyOn(window, 'fetch')
--- src/exercise/02.js
+++ src/exercise/02.js
@@ -10,74 +10,52 @@ import {
   PokemonErrorBoundary,
 } from '../pokemon'
 
-// 🐨 this is going to be our generic asyncReducer
-function pokemonInfoReducer(state, action) {
-  switch (action.type) {
+function asyncReducer(state, action) {
+  const {type, data, error} = action
+  switch (type) {
+    case 'idle': {
+      return {...state, data: null, status: 'idle', error: null}
+    }
     case 'pending': {
-      // 🐨 replace "pokemon" with "data"
-      return {status: 'pending', pokemon: null, error: null}
+      return {...state, data: null, status: 'pending', error: null}
     }
     case 'resolved': {
-      // 🐨 replace "pokemon" with "data" (in the action too!)
-      return {status: 'resolved', pokemon: action.pokemon, error: null}
+      return {...state, data, status: 'resolved', error: null}
     }
     case 'rejected': {
-      // 🐨 replace "pokemon" with "data"
-      return {status: 'rejected', pokemon: null, error: action.error}
+      return {...state, data: null, status: 'rejected', error}
     }
-    default: {
-      throw new Error(`Unhandled action type: ${action.type}`)
-    }
+    default:
+      throw new Error(`Unhandled action type:${type}`)
   }
 }
 
 function PokemonInfo({pokemonName}) {
-  // 🐨 move both the useReducer and useEffect hooks to a custom hook called useAsync
-  // here's how you use it:
-  // const state = useAsync(
-  //   () => {
-  //     if (!pokemonName) {
-  //       return
-  //     }
-  //     return fetchPokemon(pokemonName)
-  //   },
-  //   {status: pokemonName ? 'pending' : 'idle'},
-  //   [pokemonName],
-  // )
-  // 🐨 so your job is to create a useAsync function that makes this work.
-  const [state, dispatch] = React.useReducer(pokemonInfoReducer, {
-    status: pokemonName ? 'pending' : 'idle',
-    // 🐨 this will need to be "data" instead of "pokemon"
-    pokemon: null,
+  const [state, dispatch] = React.useReducer(asyncReducer, {
+    data: null,
     error: null,
+    status: 'idle',
   })
+  window.state = state
+  React.useEffect(() => console.log(state), [state])
 
   React.useEffect(() => {
-    // 💰 this first early-exit bit is a little tricky, so let me give you a hint:
-    // const promise = asyncCallback()
-    // if (!promise) {
-    //   return
-    // }
-    // then you can dispatch and handle the promise etc...
     if (!pokemonName) {
       return
     }
     dispatch({type: 'pending'})
     fetchPokemon(pokemonName).then(
       pokemon => {
-        dispatch({type: 'resolved', pokemon})
+        dispatch({type: 'resolved', data: pokemon})
       },
       error => {
         dispatch({type: 'rejected', error})
       },
     )
-    // 🐨 you'll accept dependencies as an array and pass that here.
-    // 🐨 because of limitations with ESLint, you'll need to ignore
-    // the react-hooks/exhaustive-deps rule. We'll fix this in an extra credit.
   }, [pokemonName])
 
   // 🐨 this will change from "pokemon" to "data"
-  const {pokemon, status, error} = state
+  const {data, status, error} = state
 
   if (status === 'idle' || !pokemonName) {
     return 'Submit a pokemon'
@@ -86,7 +64,7 @@ function PokemonInfo({pokemonName}) {
   } else if (status === 'rejected') {
     throw error
   } else if (status === 'resolved') {
-    return <PokemonDataView pokemon={pokemon} />
+    return <PokemonDataView pokemon={data} />
   }
 
   throw new Error('This should be impossible')
@@ -94,14 +72,8 @@ function PokemonInfo({pokemonName}) {
 
 function App() {
   const [pokemonName, setPokemonName] = React.useState('')
-
-  function handleSubmit(newPokemonName) {
-    setPokemonName(newPokemonName)
-  }
-
-  function handleReset() {
-    setPokemonName('')
-  }
+  const handleSubmit = newPokemonName => setPokemonName(newPokemonName)
+  const handleReset = () => setPokemonName('')
 
   return (
     <div className="pokemon-info-app">
