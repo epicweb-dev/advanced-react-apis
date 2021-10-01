93b67871ca8c2d8a200ff967b02efb4a0072dd0c
Ashis Kumar <37765033+ashiskumar-1999@users.noreply.github.com>
2021-10-01 22:26:10 +0530

    Learn the useReducer hook.
    Implement the useReducer hook into a Counter.  Accept step as a Action in the countReducer and change it to setState with Object.

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,16 +3,16 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+const countReducer = (state, action) => ({...state, ...action})
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+function Counter({initialCount = 0, step = 3}) {
+  const [state, setState] = React.useReducer(countReducer, {
+    count: 'initialCount',
+  })
+
+  const {count} = state
+
+  const increment = () => setState({count: count + step})
   return <button onClick={increment}>{count}</button>
 }
 
