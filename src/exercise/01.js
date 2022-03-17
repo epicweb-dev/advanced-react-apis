dd95be493eb419e6ef9d17e1053b25c1fc707fb6
luxjoshyua <josh.e.fielding@gmail.com>
2022-03-18 09:13:04 +1100

    added exercise one step one

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,18 +3,20 @@
 
 import * as React from 'react'
 
+// this is the reducer
+function countReducer(state, newState) {
+  // ignore the current state
+  return newState
+}
+
 function Counter({initialCount = 0, step = 1}) {
   // often two args passed to useReducer are state and action
   // const [state, dispatch] = React.useReducer()
   const [count, setCount] = React.useReducer(countReducer, initialCount)
 
-  // this is the reducer
-  function countReducer(count, step) {
-    return step
-  }
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount - often called an action
-  const increment = () => setCount(count + step)
+  const increment = () => setCount(count + 1)
   return <button onClick={increment}>{count}</button>
 }
 
