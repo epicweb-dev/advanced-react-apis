7d87bade69a90c19668173d73748bea999231767
luxjoshyua <josh.e.fielding@gmail.com>
2022-04-02 07:09:18 +1100

    semi finished first exercise

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -11,17 +11,50 @@ import * as React from 'react'
  *    - whatever the dispatch function (setCount) is called with - this is often called the action
  */
 
-function countReducer(count, step) {
-  // state is kind of where you're up to in the loop
-  return count + step
+// state is kind of where you're up to in the loop
+// whatever is in action will overwrite state
+// this func merges the two values (state, action) together
+// const countReducer = (state, action) => ({...state, ...action})
+
+// const countReducer = (state, action) => ({
+//   // if the action is a function, call action with state, otherwise if object call action
+//   ...state,
+//   ...(typeof action === 'function' ? action(state) : action),
+// })
+
+const countReducer = (state, action) => {
+  console.log(`state.count = ${state.count}`)
+  console.log(`action.step = ${action.step}`)
+
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + action.step,
+      }
+    default:
+      throw new Error(`Unsupported action type: ${action.type}`)
+  }
 }
 
 function Counter({initialCount = 0, step = 1}) {
   // always pass two params: the reducer and the initial state
-  const [count, changeCount] = React.useReducer(countReducer, initialCount)
+  // const [state, setState] = React.useReducer(countReducer, {
+  //   count: initialCount,
+  // })
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
 
-  // our newState is + 1 every time increment is called
-  const increment = () => changeCount(step)
+  // count is initially 0 because we've set initialCount = 0
+  const {count} = state
+
+  console.log(`count = ${count}`)
+
+  // const increment = () =>
+  //   setState(currentState => ({count: currentState.count + step}))
+
+  const increment = () => dispatch({type: 'INCREMENT', step})
+
   return <button onClick={increment}>{count}</button>
 }
 
