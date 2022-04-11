4dca52f629cca634833e9237188b71075dfbc954
Ryan Apellanes <ryanapellanes.dev@gmail.com>
2022-04-10 20:27:14 -0700

    extra 2 - setState with Object

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,13 +3,14 @@
 
 import * as React from 'react'
 
-function countReducer(count, step) {
-  return count + step
-}
+const countReducer = (state, action) => ({...state, ...action})
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, changeCount] = React.useReducer(countReducer, initialCount)
-  const increment = () => changeCount(step)
+  const [state, setState] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () => setState({count: count + step})
   return <button onClick={increment}>{count}</button>
 }
 
