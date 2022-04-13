9eaf19491d04284ec976cc6cdf38b9ab9453fe6c
luxjoshyua <josh.e.fielding@gmail.com>
2022-04-13 13:53:12 +1000

    added exercise 3 step 2, broke functions into separate folders

---
+++ src/context/count-context/index.js
@@ -0,0 +1,22 @@
+import * as React from 'react'
+
+const CountContext = React.createContext() // returns an object with two properties: {Provider, Consumer}
+
+// setup custom hook that checks for context and has error boundaries=
+export const useCount = () => {
+  const context = React.useContext(CountContext)
+  // console.log('context in useCount =', context)  = [0, f]
+  if (!context) {
+    throw new Error('useCount must be used within the CountProvider')
+  }
+  return context
+}
+
+export const CountProvider = props => {
+  // get the count state and setCount updater with React.useState
+  const [count, setCount] = React.useState(0)
+  // create a `value` array with count and setCount
+  const value = [count, setCount] // value is an integer (count) and a function (setCount)
+  // return your context provider with the value assigned to that array and forward all the other props, specifically children prop to the context provider
+  return <CountContext.Provider value={value} {...props} />
+}
--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,27 +3,19 @@
 
 import * as React from 'react'
 
-const CountContext = React.createContext()
-// returns an object with two properties: {Provider, Consumer}
+import { useCount, CountProvider } from '../context/count-context'
 
-const CountProvider = props => {
-  // get the count state and setCount updater with React.useState
-  const [count, setCount] = React.useState(0)
-  // create a `value` array with count and setCount
-  const value = [count, setCount] // value is an integer (count) and a function (setCount)
-  // return your context provider with the value assigned to that array and forward all the other props, specifically children prop to the context provider
-  return <CountContext.Provider value={value} {...props} />
-}
+// const CountContext = React.createContext([0, () => {}]) // no-op function is default set-count
 
 function CountDisplay() {
   // get the count from useContext with the CountContext
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
   // get the setCount from useContext with the CountContext
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
   // [, setCount] says skip the first item in the array which is the integer
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
@@ -32,10 +24,6 @@ function Counter() {
 function App() {
   return (
     <div>
-      {/*
-        wrap these two components in the CountProvider so they can access
-        the CountContext value
-      */}
       <CountProvider>
         <CountDisplay />
         <Counter />
