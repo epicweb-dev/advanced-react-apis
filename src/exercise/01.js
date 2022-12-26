5af697acea76ebb5814118e32bda567e8a2de7d3
Andrea Martinez <andrea.maort@gmail.com>
2022-12-26 20:39:29 +0100

    changed countReducer by returning count plus step arguments

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,19 +4,13 @@
 import * as React from 'react'
 
 function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  function countReducer(state, newState) {
-    return newState
+  function countReducer(count, step) {
+    return count + step
   }
 
   const [count, setCount] = React.useReducer(countReducer, initialCount)
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+  const increment = () => setCount(step)
   return <button onClick={increment}>{count}</button>
 }
 
