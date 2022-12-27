0685a1732991631e7d74f156c7983585be395b97
Andrea Martinez <andrea.maort@gmail.com>
2022-12-27 11:03:23 +0100

    added useCallback hook

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,6 +2,7 @@
 // http://localhost:3000/isolated/exercise/02.js
 
 import * as React from 'react'
+import {useCallback} from 'react'
 import {
   fetchPokemon,
   PokemonForm,
@@ -28,7 +29,7 @@ function asyncReducer(state, action) {
   }
 }
 
-function useAsync(asyncCallback, initialState, dependencies) {
+function useAsync(asyncCallback, initialState) {
   const [state, dispatch] = React.useReducer(asyncReducer, {
     status: 'idle',
     data: null,
@@ -50,20 +51,21 @@ function useAsync(asyncCallback, initialState, dependencies) {
         dispatch({type: 'rejected', error})
       },
     )
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, dependencies)
+  }, [asyncCallback])
 
   return state
 }
 
 function PokemonInfo({pokemonName}) {
+  const asyncCallback = useCallback(() => {
+    if (!pokemonName) {
+      return
+    }
+    return fetchPokemon(pokemonName)
+  }, [pokemonName])
+
   const state = useAsync(
-    () => {
-      if (!pokemonName) {
-        return
-      }
-      return fetchPokemon(pokemonName)
-    },
+    asyncCallback,
     {
       status: pokemonName ? 'pending' : 'idle',
     },
