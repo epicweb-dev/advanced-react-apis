57461bb879c52df97fb7930ca4c864603b3cdbd2
Stefano Ramirez <ramirez.stefano@gmail.com>
2023-03-06 10:58:41 -0600

    ex1 extra credit 1 changing the reducer hook to adapt to the API and accept the action as the required argument.

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,19 +4,22 @@
 import * as React from 'react'
 
 
-const countReducer = (state, newState) => newState
+const countReducer = (count, change) => count + change
+
 
 function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
+  const [count, changeCount] = React.useReducer(countReducer, initialCount)
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   
 
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+
+  //extra credit 1 moddifications to change the ste into an acction and pass it.
+  const increment = () => changeCount(step)
   return <button onClick={increment}>{count}</button>
 }
 
