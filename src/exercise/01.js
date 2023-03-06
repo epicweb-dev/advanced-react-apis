010f56b9b6e34afc8374adcc0389a1660b565632
Stefano Ramirez <ramirez.stefano@gmail.com>
2023-03-06 11:29:04 -0600

    Ex1 with extra credit 4 done, implementing the traditional dispatch object with a type and switch statement

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -7,17 +7,26 @@ import * as React from 'react'
 // changing the state to an object e.g. {count: 0} extra credit 2
 
 //extra credit 3 modding to accept a funtion to support both an object and an option.
-const countReducer = (state, action) => ({
-  ...state,
-   ...(typeof action === 'function' ? action(state) : state)
-   /*
-   if(typeof action === 'function){
-    action(state) //function call.
-   } else {
-    state //object call.
-   }
-   */
-  })
+
+//extra credit 4 implementation with a dispatch object and a switch statement.
+function countReducer(state, action){ //change from const to function.
+  const {type, step} = action //object with both type and step variables.
+
+  switch(type){
+    case 'INCREMENT': {
+
+      return {
+        ...state,
+        count: state.count + step,
+      }
+    }
+    default: {
+      throw new Error(`Unsupported action type: ${type}`)
+    }
+
+  }
+
+  }
 
 
 function Counter({initialCount = 0, step = 1}) {
@@ -25,12 +34,11 @@ function Counter({initialCount = 0, step = 1}) {
   // 💰 React.useReducer(countReducer, initialCount)
 
   //extra crdit 2 API, must addapt the reducer hook.
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
   
   return <button onClick={increment}>{count}</button>
 }
