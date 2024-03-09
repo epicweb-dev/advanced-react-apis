477449ce521d285d1875c9ae10f113cb848e9546
Kent C. Dodds <me+github@kentcdodds.com>
2024-03-09 15:56:00 -0700

    finish exercise 9

--- exercises/09.sync-external/01.problem.sub/index.tsx
+++ exercises/09.sync-external/01.problem.sub/index.tsx
@@ -1,6 +1,17 @@
 import * as ReactDOM from 'react-dom/client'
 
+// 💰 this is the mediaQuery we're going to be matching against:
+// const mediaQuery = '(max-width: 600px)'
+
+// 🐨 make a getSnapshot function here that returns whether the media query matches
+
+// 🐨 make a subscribe function here which takes a callback function
+// 🐨 create a matchQueryList variable here with the mediaQuery from above (📜 https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList)
+// 🐨 add a change listener to the mediaQueryList which calls the callback
+// 🐨 return a cleanup function whihc removes the change event listener for the callback
+
 function NarrowScreenNotifier() {
+	// 🐨 assign this to useSyncExternalStore with the subscribe and getSnapshot functions above
 	const isNarrow = false
 	return isNarrow ? 'You are on a narrow screen' : 'You are on a wide screen'
 }
--- exercises/09.sync-external/02.problem.util/index.tsx
+++ exercises/09.sync-external/02.problem.util/index.tsx
@@ -2,6 +2,10 @@ import { useSyncExternalStore } from 'react'
 import * as ReactDOM from 'react-dom/client'
 
 const mediaQuery = '(max-width: 600px)'
+
+// 🐨 put getSnapshot and subscribe in a new function called makeMediaQueryStore
+// which accepts a mediaQuery and returns a hook that uses useSyncExternalStore
+// with the subscribe and getSnapshot functions.
 function getSnapshot() {
 	return window.matchMedia(mediaQuery).matches
 }
@@ -13,8 +17,13 @@ function subscribe(callback: () => void) {
 		mediaQueryList.removeEventListener('change', callback)
 	}
 }
+// 🐨 put everything above in the makeMediaQueryStore function
+
+// 🐨 call makeMediaQueryStore with '(max-width: 600px)' and assign the return
+// value to a variable called useNarrowMediaQuery
 
 function NarrowScreenNotifier() {
+	// 🐨 call useNarrowMediaQuery here instead of useSyncExternalStore
 	const isNarrow = useSyncExternalStore(subscribe, getSnapshot)
 	return isNarrow ? 'You are on a narrow screen' : 'You are on a wide screen'
 }
