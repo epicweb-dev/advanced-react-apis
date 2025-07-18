1149b2125728dcdafbc83a1f98a5ee997c6cd173
Kent C. Dodds <me+github@kentcdodds.com>
2025-07-17 20:35:44 -0600

    update setup

--- epicshop/setup-custom.js
+++ epicshop/setup-custom.js
@@ -1,10 +1,13 @@
 import path from 'node:path'
+import { warm } from '@epic-web/workshop-cli/warm'
 import {
 	getApps,
 	isProblemApp,
 	setPlayground,
 } from '@epic-web/workshop-utils/apps.server'
 import fsExtra from 'fs-extra'
+
+await warm()
 
 const allApps = await getApps()
 const problemApps = allApps.filter(isProblemApp)
